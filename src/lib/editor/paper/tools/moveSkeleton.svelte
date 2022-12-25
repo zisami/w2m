35b7c76f4ab4e2e8b692ab4fb61f35f77d1a7e71
zisami <sascha.zika@gmx.at>
2022-12-25 11:20:22 +0100

    refactor to typescript #2 tests skeleton

---
+++ src/lib/editor/paper/tools/moveSkeleton.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import paper from 'paper';
+
+	interface moveLayer extends paper.Tool {
+		name: string;
+	}
+
+	const toolName = 'moveSkeleton';
+	let clickedLayer: paper.Layer | null = null;
+    let clickedItem: paper.Item | null = null;
+    const skeletonConfig = {
+        lowerLegs:{
+            length: {
+                min: 15,
+                max: 30,
+                original: 100,
+            }
+        }
+    }
+
+	onMount(() => {
+		const tool: moveLayer = new paper.Tool();
+		tool.name = toolName;
+		tool.onMouseDown = function (event: paper.ToolEvent) {
+			// Test for a hit on any layer
+			let hitResult = paper.project.hitTest(event.point);
+			if (hitResult?.item) {
+				const familyTree = [hitResult.item.name];
+				let parent = hitResult.item.parent;
+				if (parent) {
+					familyTree.push(parent.name);
+				}
+				while (!(parent instanceof paper.Layer)) {
+					parent = parent.parent;
+					familyTree.push(parent.name);
+				}
+				// Get the clicked layer
+				clickedLayer = parent;
+                clickedItem = hitResult.item;
+				clickedItem.selected = true;
+				console.log(familyTree);
+			}
+		};
+
+		tool.onMouseDrag = function (event: paper.ToolEvent) {
+			// Move all the items on the clicked layer to the clicked point
+			if (clickedItem) {
+                console.log(event.point.getDistance(clickedItem.parent.position));
+                
+					clickedItem.position = event.point;
+			}
+		};
+		tool.onMouseUp = function () {
+			if (clickedLayer) clickedLayer.selected = false;
+		};
+
+		tool.activate();
+	});
+	const onActivate = () => {
+		const tool = paper.tools.find((tool) => {
+			if ('name' in tool) {
+				//console.log('tool.name', tool.name, toolName);
+				return tool.name === toolName;
+			}
+			return false;
+		});
+        paper.project.layers.find((layer) => layer.name === 'skeletonDots')?.bringToFront()
+		tool?.activate();
+	};
+</script>
+
+<div class="button rounded-full bg-orange-900 text-orange-200 px-3 py-1" on:click={onActivate} on:keydown={onActivate}>Skeleton</div>
